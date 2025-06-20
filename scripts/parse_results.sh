1cd04ddc8d23af22b8775e533a5c62d6ae7faebd
alibrahimzada <alibrahimzada@gmail.com>
2025-06-20 18:51:14 +0000

    add parsing script

---
+++ scripts/parse_results.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export PYTHONPATH=$(pwd):$PYTHONPATH
+python3 src/parse_results/extract_rustrepotrans.py
+python3 src/parse_results/extract_alphatrans.py
+# TODO: add more tools here
+
+# note: there might be a need to manually fix some bad rustrepotrans results. do it before running the next command
+for agent in base_agent match_agent; do
+    for tool in alphatrans rustrepotrans; do
+        mkdir -p data/agent_results/$agent/$tool
+        cp -r data/tool_results/$tool/processed_results/*.json data/agent_results/$agent/$tool
+        cp -r data/tool_results/$tool/processed_results/*.json data/agent_results/$agent/$tool
+    done
+done
