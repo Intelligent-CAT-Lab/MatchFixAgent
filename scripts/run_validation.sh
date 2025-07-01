481ded5116ddc3f8711dcc949d6cc1f90735be97
alibrahimzada <alibrahimzada@gmail.com>
2025-07-01 14:55:54 +0000

    update

--- scripts/run_validation.sh
+++ scripts/run_validation.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PROJECT_NAME=$1
-CONFIG_FILE_NAME=$2
+CONFIG_FILE_PATH=$1
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
-python3 src/validator/validate.py --config_file=configs/$CONFIG_FILE_NAME.yaml --project_name=$PROJECT_NAME
+python3 src/validator/validate.py --config_file=$CONFIG_FILE_PATH
