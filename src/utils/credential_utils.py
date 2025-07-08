36ba8d5ecbdecabf6dcb43bd09f544a60d4f0b14
alibrahimzada <alibrahimzada@gmail.com>
2025-07-08 20:25:47 +0000

    add go and node paths to env

--- src/utils/credential_utils.py
+++ src/utils/credential_utils.py
@@ -188,4 +188,10 @@ def setup_environment_for_agent(
     # Add cargo to PATH
     env["PATH"] = f"{os.path.expanduser('~/.cargo/bin')}:{env['PATH']}"
 
+    # Add go to PATH
+    env["PATH"] = f"{os.path.expanduser('/usr/local/go/bin')}:{env['PATH']}"
+
+    # Add node to PATH
+    env["PATH"] = f"{os.path.expanduser('~/.nvm/versions/node/v22.16.0/bin')}:{env['PATH']}"
+
     return env, credential_name
