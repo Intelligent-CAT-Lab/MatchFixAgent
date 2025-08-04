938a26f1d2efcdcd3122fbee399f14ed38169b14
alibrahimzada <alibrahimzada@gmail.com>
2025-08-04 03:58:23 +0000

    update Dockerfile

--- docker-env/docker_run.sh
+++ docker-env/docker_run.sh
@@ -4,9 +4,8 @@ if [ "${PWD: -10}" != "docker-env" ]; then
     exit 1
 fi
 
-./docker_stop.sh
 docker run --name matchfixagent_container -d -t \
     --mount type=bind,source=$PWD/..,target=/workspace/ \
     matchfixagent
 docker ps | grep matchfixagent_container
-echo "# The container is running. To stop running the container: ./docker_stop.sh"
+echo "# The container is running. To stop running the container: bash docker_stop.sh"
