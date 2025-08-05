7d5ff0119f7a685d219e3141754019949334eaee
alibrahimzada <alibrahimzada@gmail.com>
2025-08-05 20:18:15 +0000

    fix

--- scripts/parse_results.sh
+++ scripts/parse_results.sh
@@ -6,7 +6,7 @@ python3 src/parse_results/extract_alphatrans.py
 # TODO: add more tools here
 
 # note: there might be a need to manually fix some bad rustrepotrans results. do it before running the next command
-for agent in base_agent match_agent; do
+for agent in base_agent match_agent openai_agent; do
     for tool in alphatrans rustrepotrans oxidizer skel; do
         mkdir -p data/agent_results/$agent/$tool
         cp -r data/tool_results/$tool/processed_results/*.json data/agent_results/$agent/$tool
